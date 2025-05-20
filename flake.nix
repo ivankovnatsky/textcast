3de39ea04004edd42eb01b6cca03fb1f9a3b9a06
Ivan Kovnatsky <75213+ivankovnatsky@users.noreply.github.com>
2025-05-20 21:01:19 +0300

    .secrets: init

--- flake.nix
+++ flake.nix
@@ -56,8 +56,8 @@
             shellHook = ''
               export PLAYWRIGHT_BROWSERS_PATH="${playwright-browsers-chromium}"
 
-              export OPENAI_API_KEY=$(pass openai-api-key)
-              export ELEVEN_API_KEY=$(pass eleven-api-key)
+              export OPENAI_API_KEY=$(cat .secrets/openai-api-key)
+              export ELEVEN_API_KEY=$(cat .secrets/eleven-api-key)
             '';
           };
         }
